6edd27e1d28bfecc13bb6b3fd48352db1e3eafc3
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-07-27 11:46:17 -0300

    Product

--- backend/src/main/java/com/rycardofarias/com/backend/config/TestConfig.java
+++ backend/src/main/java/com/rycardofarias/com/backend/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.rycardofarias.com.backend.config;
 
 import com.rycardofarias.com.backend.entities.Category;
 import com.rycardofarias.com.backend.entities.Order;
+import com.rycardofarias.com.backend.entities.Product;
 import com.rycardofarias.com.backend.entities.User;
 import com.rycardofarias.com.backend.entities.enuns.OrderStatus;
 import com.rycardofarias.com.backend.repositories.CategoryRepository;
 import com.rycardofarias.com.backend.repositories.OrderRepository;
+import com.rycardofarias.com.backend.repositories.ProductRepository;
 import com.rycardofarias.com.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -35,7 +40,14 @@ public class TestConfig implements CommandLineRunner{
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Brown", "alex@gmail.com", "988888887", "123456");
---
+++ backend/src/main/java/com/rycardofarias/com/backend/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.rycardofarias.com.backend.services;
+
+import com.rycardofarias.com.backend.entities.Product;
+import com.rycardofarias.com.backend.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
