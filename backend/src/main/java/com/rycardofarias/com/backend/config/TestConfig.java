188a0205964f99bfa78a49778f1097fe742960ed
Ricardo Farias <ricardofariasxyz@gmail.com>
2021-07-27 09:08:22 -0300

    OrderStatus enum

--- backend/src/main/java/com/rycardofarias/com/backend/config/TestConfig.java
+++ backend/src/main/java/com/rycardofarias/com/backend/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.rycardofarias.com.backend.config;
 
 import com.rycardofarias.com.backend.entities.Order;
 import com.rycardofarias.com.backend.entities.User;
+import com.rycardofarias.com.backend.entities.enuns.OrderStatus;
 import com.rycardofarias.com.backend.repositories.OrderRepository;
 import com.rycardofarias.com.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Brown", "alex@gmail.com", "988888887", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- backend/src/main/java/com/rycardofarias/com/backend/entities/Order.java
+++ backend/src/main/java/com/rycardofarias/com/backend/entities/Order.java
@@ -2,6 +2,7 @@ package com.rycardofarias.com.backend.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rycardofarias.com.backend.entities.enuns.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -21,6 +22,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -29,10 +32,46 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
         this.client = client;
     }
 
